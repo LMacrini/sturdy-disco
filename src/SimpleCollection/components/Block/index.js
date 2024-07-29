8be6cd1ac3c9e9bf5990cdb116385e6590103181
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-07-29 15:40:32 -0400

    Update index.js

--- src/SimpleCollection/components/Block/index.js
+++ src/SimpleCollection/components/Block/index.js
@@ -4,7 +4,7 @@ export default (props) => {
     console.log('Congrats, the setup is successful!');
     console.log('Props received are: ', props);
     let title = props.block.main.header.title;
-    console.log(title);
+    console.log(title ? title : "Hello World");
 
     return <p className='text-lg text-gray-700 font-bold px-4 py-2 bg-gray-100'>{!!title ? title : "Hello World"}</p>;
 };
