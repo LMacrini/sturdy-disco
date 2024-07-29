47eec0d0fd8bb7550ae761b99d3b84fad9f98661
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-07-29 15:35:52 -0400

    Update index.js

--- src/SimpleCollection/components/Block/index.js
+++ src/SimpleCollection/components/Block/index.js
@@ -6,5 +6,5 @@ export default (props) => {
     let title = props.block.main.header.title;
     console.log(title);
 
-    return <p className='text-lg text-gray-700 font-bold px-4 py-2 bg-gray-100'>{title !== undefined ? title : "Hello World"}</p>;
+    return <p className='text-lg text-gray-700 font-bold px-4 py-2 bg-gray-100'>{!!title ? title : "Hello World"}</p>;
 };
