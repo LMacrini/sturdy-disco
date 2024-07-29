cf56e08fea530cbce2a20e2f700691e61b14c9be
LMacrini <62901118+LMacrini@users.noreply.github.com>
2024-07-29 15:47:18 -0400

    Update index.js

--- src/SimpleCollection/components/Block/index.js
+++ src/SimpleCollection/components/Block/index.js
@@ -3,8 +3,7 @@ import React from 'react';
 export default (props) => {
     console.log('Congrats, the setup is successful!');
     console.log('Props received are: ', props);
-    let title = props.block.main.header.title;
-    console.log(title ? title : "Hello World");
+    console.log(props.block.main.header.title || "Hello World");
 
-    return <p className='text-lg text-gray-700 font-bold px-4 py-2 bg-gray-100'>{!!title ? title : "Hello World"}</p>;
+    return <p className='text-lg text-gray-700 font-bold px-4 py-2 bg-gray-100'>{props.block.main.header.title || "Hello World"}</p>;
 };
